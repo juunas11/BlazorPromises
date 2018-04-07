2e6beaa9b697fd6e7ea0f56cef982b770ae3226a
Joonas Westlin <joonas.westlin@kompozure.net>
2018-04-07 09:46:12 +0000

    Refactored out all the reflection stuff. Cleaned up files.

---
+++ PromiseBlazorTest/PromiseCallbackHandler.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Blazor;
+using System;
+using System.Threading.Tasks;
+
+namespace PromiseBlazorTest
+{
+    public class PromiseCallbackHandler<TResult> : IPromiseCallbackHandler
+    {
+        private readonly TaskCompletionSource<TResult> _tcs;
+
+        public PromiseCallbackHandler(TaskCompletionSource<TResult> tcs)
+        {
+            _tcs = tcs;
+        }
+
+        public void SetResult(string json)
+        {
+            TResult result = JsonUtil.Deserialize<TResult>(json);
+            _tcs.SetResult(result);
+        }
+
+        public void SetError(string error)
+        {
+            var exception = new Exception(error);
+            _tcs.SetException(exception);
+        }
+    }
+}
---
+++ PromiseBlazorTest/Promises.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Blazor.Browser.Interop;
+using System;
+using System.Collections.Concurrent;
+using System.Threading.Tasks;
+
+namespace PromiseBlazorTest
+{
+    public static class Promises
+    {
+        private static ConcurrentDictionary<string, IPromiseCallbackHandler> CallbackHandlers =
+            new ConcurrentDictionary<string, IPromiseCallbackHandler>();
+
+        public static void PromiseCallback(string callbackId, string result)
+        {
+            if(CallbackHandlers.TryGetValue(callbackId, out IPromiseCallbackHandler handler))
+            {
+                handler.SetResult(result);
+                CallbackHandlers.TryRemove(callbackId, out IPromiseCallbackHandler _);
+            }
+        }
+
+        public static void PromiseError(string callbackId, string error)
+        {
+            if (CallbackHandlers.TryGetValue(callbackId, out IPromiseCallbackHandler handler))
+            {
+                handler.SetError(error);
+                CallbackHandlers.TryRemove(callbackId, out IPromiseCallbackHandler _);
+            }
+        }
+
+        public static Task<TResult> ExecuteAsync<TResult>(string fnName, object data = null)
+        {
+            var tcs = new TaskCompletionSource<TResult>();
+            
+            string callbackId = Guid.NewGuid().ToString();
+            if(CallbackHandlers.TryAdd(callbackId, new PromiseCallbackHandler<TResult>(tcs)))
+            {
+                if (data == null)
+                {
+                    RegisteredFunction.Invoke<bool>("runFunction", callbackId, fnName);
+                }
+                else
+                {
+                    RegisteredFunction.Invoke<bool>("runFunction", callbackId, fnName, data);
+                }
+
+                return tcs.Task;
+            }
+            throw new Exception("An entry with the same callback id already existed, really should never happen");
+        }
+    }
+}
